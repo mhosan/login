a18e707d64604c549c95782c9a516cef2ecb37d4
Hosan Marcelo <marcelo.hosan@arba.gov.ar>
2019-03-20 17:24:57 -0300

    agregado de las credenciales de firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyBUxp7pkqLENs2MgZdZtQV6CZ7A5offjSQ",
+    authDomain: "datosweb-624fb.firebaseapp.com",
+    databaseURL: "https://datosweb-624fb.firebaseio.com",
+    projectId: "datosweb-624fb",
+    storageBucket: "datosweb-624fb.appspot.com",
+    messagingSenderId: "81794520230"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyBUxp7pkqLENs2MgZdZtQV6CZ7A5offjSQ",
+    authDomain: "datosweb-624fb.firebaseapp.com",
+    databaseURL: "https://datosweb-624fb.firebaseio.com",
+    projectId: "datosweb-624fb",
+    storageBucket: "datosweb-624fb.appspot.com",
+    messagingSenderId: "81794520230"
+  }
 };
 
 /*
